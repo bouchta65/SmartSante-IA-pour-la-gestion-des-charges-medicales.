878e1caf08e6c956bb90708118604e86a527740e
Bouchta Mohamed <bouchtamohamed01@gmail.com>
2025-09-29 12:02:01 +0100

    Prétraitement des données : conversion de 'sex' et 'smoker' en 0/1 et encodage de 'region' en variables dummy

---
+++ src/data_preparation.py
@@ -0,0 +1,34 @@
+import pandas as pd
+
+path = r"data\assurance-maladie-68d92978e362f464596651.csv"
+content = pd.read_csv(path)
+
+content.columns
+content.head()
+content.info()
+
+# Doc : Après vérification, j'ai vu que les colonnes 'Sex' (valeurs : male/female), 
+# 'Smoker' (valeurs : yes/no) et 'Region' (valeurs : northeast/southeast) 
+# doivent être converties en int (0 ou 1) et non laissées en objets (object).
+
+content['sex'] = content['sex'].map({'male':1,'female':0})
+content['smoker'] = content['smoker'].map({'yes':1,'no':0})
+# content['region'] = content['region'].map({'southwest':1,'southeast':2,'northwest':3,'northeast':4})
+content = pd.get_dummies(content,columns=['region'])
+
+content['region_northeast'] = content['region_northeast'].astype(int)
+content['region_northwest'] = content['region_northwest'].astype(int)
+content['region_southeast'] = content['region_southeast'].astype(int)
+content['region_southwest'] = content['region_southwest'].astype(int)
+
+# Convert boolean values (True/False) to numeric (1/0) 
+# because machine learning models require numeric input to perform calculations.
+
+print(content.info())
+print(content.head())
+print(content.tail())
+
+meanAge = content['age'].mean()
+print(round(meanAge,2))
+
+
